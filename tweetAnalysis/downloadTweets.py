c6b79f3367153deb5d96c849ac60ecb2cbfe1ff4
Jayesh Srivastava <jayesh0200@gmail.com>
2021-07-20 00:49:18 +0530

    Update downloadTweets.py

--- tweetAnalysis/downloadTweets.py
+++ tweetAnalysis/downloadTweets.py
@@ -38,15 +38,15 @@ with open(fName, 'w') as f:
 
                 else:
                     new_tweets = api.search(q=q, lang="en", count=tweetsPerQry,
-                                        since_id=sinceId, tweet_mode='extended')
+                                 since_id=sinceId, tweet_mode='extended')
             else:
                 if not sinceId:
                     new_tweets = api.search(q=q, lang="en", count=tweetsPerQry,
-                                        max_id=str(max_id - 1), tweet_mode='extended')
+                                 max_id=str(max_id - 1), tweet_mode='extended')
                 else:
                     new_tweets = api.search(q=q, lang="en", count=tweetsPerQry,
-                                        max_id=str(max_id - 1),
-                                        since_id=sinceId, tweet_mode='extended')
+                                 max_id=str(max_id - 1),
+                                 since_id=sinceId, tweet_mode='extended')
 
             if not new_tweets:
                 print("No more tweets found")
